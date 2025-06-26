7fac83d82702171923bae22b045702e017da1c73
Chern <781327871@qq.com>
2025-06-26 22:23:36 +0800

    feat(clean user page): clean default cotent of example in user page

--- src/pages/user/constants.ts
+++ src/pages/user/constants.ts
@@ -23,48 +23,14 @@ export const USER_INFO_LIST: Array<UserInfoListType> = [
   },
   {
     title: 'pages.user.personalInfo.desc.entity',
-    content: '腾讯集团',
-  },
-  {
-    title: 'pages.user.personalInfo.desc.leader',
-    content: 'Michael Wang',
+    content: 'Chern Blog',
   },
   {
     title: 'pages.user.personalInfo.desc.position',
-    content: '高级 UI 设计师',
+    content: '管理员',
   },
   {
     title: 'pages.user.personalInfo.desc.joinDay',
     content: '2021-07-01',
   },
-  {
-    title: 'pages.user.personalInfo.desc.group',
-    content: '腾讯/腾讯公司/某事业群/某产品部/某运营中心/商户服务组',
-    span: 6,
-  },
 ];
-
-export const TEAM_MEMBERS = [
-  {
-    avatar: 'https://avatars.githubusercontent.com/Wen1kang',
-    title: 'Lovellzhong 钟某某',
-    description: '直客销售 港澳拓展组员工',
-  },
-  {
-    avatar: 'https://avatars.githubusercontent.com/pengYYYYY',
-    title: 'Jiajingwang 彭某某',
-    description: '前端开发 前台研发组员工',
-  },
-  {
-    avatar: 'https://avatars.githubusercontent.com/u/24469546?s=96&v=4',
-    title: 'cruisezhang 林某某',
-    description: '技术产品 产品组员工',
-  },
-  {
-    avatar: 'https://avatars.githubusercontent.com/u/88708072?s=96&v=4',
-    title: 'Lovellzhang 商某某',
-    description: '产品运营 港澳拓展组员工',
-  },
-];
-
-export const PRODUCT_LIST = ['a', 'b', 'c', 'd'];
--- src/pages/user/index.vue
+++ src/pages/user/index.vue
@@ -21,64 +21,13 @@
           </t-descriptions-item>
         </t-descriptions>
       </t-card>
-
-      <t-card class="content-container" :bordered="false">
-        <t-tabs value="second">
-          <t-tab-panel value="first" :label="t('pages.user.contentList')">
-            <p>{{ t('pages.user.contentList') }}</p>
-          </t-tab-panel>
-          <t-tab-panel value="second" :label="t('pages.user.contentList')">
-            <t-card :bordered="false" class="card-padding-no" :title="t('pages.user.visitData')" describe="（次）">
-              <template #actions>
-                <t-date-range-picker
-                  class="card-date-picker-container"
-                  :default-value="LAST_7_DAYS"
-                  theme="primary"
-                  mode="date"
-                  @change="onLineChange"
-                />
-              </template>
-              <div id="lineContainer" style="width: 100%; height: 328px" />
-            </t-card>
-          </t-tab-panel>
-          <t-tab-panel value="third" :label="t('pages.user.contentList')">
-            <p>{{ t('pages.user.contentList') }}</p>
-          </t-tab-panel>
-        </t-tabs>
-      </t-card>
     </t-col>
 
     <t-col :flex="1">
       <t-card class="user-intro" :bordered="false">
-        <t-avatar size="80px">T</t-avatar>
-        <div class="name">My Account</div>
-        <div class="position">{{ t('pages.user.personalInfo.position') }}</div>
-      </t-card>
-
-      <t-card :title="t('pages.user.teamMember')" class="user-team" :bordered="false">
-        <template #actions>
-          <t-button theme="default" shape="square" variant="text">
-            <t-icon name="ellipsis" />
-          </t-button>
-        </template>
-        <t-list :split="false">
-          <t-list-item v-for="(item, index) in TEAM_MEMBERS" :key="index">
-            <t-list-item-meta :image="item.avatar" :title="item.title" :description="item.description" />
-          </t-list-item>
-        </t-list>
-      </t-card>
-
-      <t-card :title="t('pages.user.serviceProduction')" class="product-container" :bordered="false">
-        <template #actions>
-          <t-button theme="default" shape="square" variant="text">
-            <t-icon name="ellipsis" />
-          </t-button>
-        </template>
-        <t-row class="content" :getters="16">
-          <t-col v-for="(item, index) in PRODUCT_LIST" :key="index" :span="3">
-            <component :is="getIcon(item)"></component>
-          </t-col>
-        </t-row>
+        <t-avatar size="80px">CB</t-avatar>
+        <div class="name">Chern</div>
+        <div class="position">chern blog manager管理员</div>
       </t-card>
     </t-col>
   </t-row>
@@ -88,19 +37,13 @@ import { LineChart } from 'echarts/charts';
 import { GridComponent, LegendComponent, TooltipComponent } from 'echarts/components';
 import * as echarts from 'echarts/core';
 import { CanvasRenderer } from 'echarts/renderers';
-import type { DateRangeValue } from 'tdesign-vue-next';
 import { computed, nextTick, onMounted, onUnmounted, watch } from 'vue';
 
-import ProductAIcon from '@/assets/assets-product-1.svg';
-import ProductBIcon from '@/assets/assets-product-2.svg';
-import ProductCIcon from '@/assets/assets-product-3.svg';
-import ProductDIcon from '@/assets/assets-product-4.svg';
 import { t } from '@/locales';
 import { useSettingStore } from '@/store';
 import { changeChartsTheme } from '@/utils/color';
-import { LAST_7_DAYS } from '@/utils/date';
 
-import { PRODUCT_LIST, TEAM_MEMBERS, USER_INFO_LIST } from './constants';
+import { USER_INFO_LIST } from './constants';
 import { getFolderLineDataSet } from './index';
 
 defineOptions({
@@ -113,15 +56,6 @@ let lineContainer: HTMLElement;
 let lineChart: echarts.ECharts;
 const store = useSettingStore();
 const chartColors = computed(() => store.chartColors);
-
-const onLineChange = (value: DateRangeValue) => {
-  lineChart.setOption(
-    getFolderLineDataSet({
-      dateTime: value as string[],
-      ...chartColors.value,
-    }),
-  );
-};
 
 const initChart = () => {
   lineContainer = document.getElementById('lineContainer');
@@ -154,21 +88,6 @@ onMounted(() => {
 onUnmounted(() => {
   window.removeEventListener('resize', updateContainer);
 });
-
-const getIcon = (type: string) => {
-  switch (type) {
-    case 'a':
-      return ProductAIcon;
-    case 'b':
-      return ProductBIcon;
-    case 'c':
-      return ProductCIcon;
-    case 'd':
-      return ProductDIcon;
-    default:
-      return ProductAIcon;
-  }
-};
 
 watch(
   () => store.brandTheme,
